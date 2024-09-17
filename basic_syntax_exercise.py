5ee2e52ccae11ebf7f41aed92f8496559f345395
Khens96 <Khensaneo@icloud.com>
2024-09-17 08:35:54 +0200

    saving changes

--- basic_syntax_exercise.py
+++ basic_syntax_exercise.py
@@ -3,35 +3,35 @@
 # 1. Variables: You can store values in variables.
 
 # On the next line store the number '10' in the variable 'x'.
-
+x = 10
 
 # On the next line, store the number '20.5' in the variable 'y'.
-
+y = 20.5
 
 # On the next line, store the string 'Hello, World!' in the variable 'z'.
-
+z = "Hello, World!"
 
 # On the next line, store the boolean 'True' in the variable 'a'.
-
+a = True
 
 # On the next line, store the boolean 'False' in the variable 'b'.
-
+b = False
 
 # Use the print statement and the variable 'x' to display the message:
     # "10, is an integer variable."
-print()    
+print(x, ",is an integer variable. ")    
 # Use the print statement and the variable 'y' to display the message: 
     # "20.5, is a float variable."
-print()
+print(y, ",is a float variable.")
 # Use the print statement and the variable 'z' to display the message: 
     # " 'Hello, World!' is a string variable."
-print()
+print(z, ",is a string variable.")
 # Use the print statement and the variable 'a' to display the message:
     # " 'True', is a boolean variable."
-print()
+print(f"'{a}', is a boolean variable." )
 # Use the print statement and the variable 'b' to display the message:
     # " 'False', is a boolean variable.
-print()    
+print(f"'{b}', is a boolean variable.")    
 
 
 # 2. Data Types: Python has several built-in data types.
@@ -42,23 +42,23 @@ print()
 
 # Edit the code below to print:
     # "10 belongs to <class 'int'>: meaning it is an integer."
-print()
+print(x, 'belongs to', type(x), 'meaning it is an integer.')
 
 # Edit the code below to print:
     # " 20.5 belongs to <class 'float'>: meaning it is a float."
-print()
+print(y, 'belongs to', type(y), 'meaning it is a float.')
 
 # Edit the code below to print:
     # " 'Hello, World!', belongs to <class 'str'>: meaning it is a string."
-print()
+print(f"'{z}', 'belongs to' {type(z)}: meaning it is a string.")
 
 # Edit the code below to print:
     # "'True', belongs to <class 'bool'>: meaning it is a boolean."
-print()
+print(f"'{a}', 'belongs to' {type(a)}: meaning it is a boolean.")
 
 # Edit the code below to print:
     # "'False', belongs to <class 'bool'>: meaning it is a boolean.
-print()
+print(f"'{b}', 'belongs to' {type(b)}: meaning it is a boolean.")
 
 
 # 3. Basic Operators: 
@@ -66,19 +66,19 @@ print()
 # Arithmetic Operators
 
 # Addition: print the sum of 'x' and 'y'.
-print("The sum of 'x' and 'y' is: ")
+print("The sum of 'x' and 'y' is: ", x + y)
 
 # Subtraction: print the difference between 'x' and 'y'.
-print("The difference between 'x' and 'y' is: ")
+print("The difference between 'x' and 'y' is: ",x - y)
 
 # Multiplication: print the product of 'x' and 'y'.
-print("The product of 'x' and 'y' is: ")
+print("The product of 'x' and 'y' is: ", x * y)
 
 # Division: print the answer when you divide 'x' by 'y'.
-print("The division of 'x' by 'y' is:")
+print("The division of 'x' by 'y' is:", x / y)
 
 # Modulus (remainder of the division): print the remainder after dividing 'x' by 'y'.
-print("The modulus of 'x' and 'y' is: ")
+print("The modulus of 'x' and 'y' is: ", x % y)
 
 
 # 4. Indentation: 
@@ -88,4 +88,5 @@ print("The modulus of 'x' and 'y' is: ")
 
 # for number in range(6):
 # print()  # This line is part of the for loop and it must be indented.
-    
+for i in range(6):
+    print()
